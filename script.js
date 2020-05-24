4da2b5e32701ec17b64367d38ef86219b165c26f
Glitch (as91897) <none>
2020-05-24 22:25:26 +0000

    💡🎙 Updated with Glitch

--- script.js
+++ script.js
@@ -3,3 +3,12 @@
 
 // prints "hi" in the browser's dev tools console
 console.log("hi");
+
+function myFunction() {
+  var x = document.getElementById("demo");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
